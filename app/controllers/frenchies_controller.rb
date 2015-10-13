3cea243a71e5a55c9fcbabea088bb687cd6fafd4
Ricardo Parro <ricardo.parro@gmail.com>
2015-10-13 23:40:08 +0200

    giphy is working added telegram bot api

--- app/controllers/frenchies_controller.rb
+++ app/controllers/frenchies_controller.rb
@@ -1,8 +1,25 @@
+require 'telegram/bot'
+
 class FrenchiesController < ApplicationController
 
 
-def index
-@frenchies = Giphy.trending(limit: 5) 
-end
+
+    def index
+
+        @frenchies = Giphy.search('french bulldog', {limit: 50, offset: 25})
+        token = '109398535:AAFzTWO4AtPAnWOuhRsCN5YlfUavWOBaQ_4'
+        Telegram::Bot::Client.run(token) do |bot|
+            bot.listen do |message|
+                case message.text
+                when '/start'
+                    bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+                when '/stop'
+                    bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+                end
+            end
+        end
+
+
+    end
 
 end
